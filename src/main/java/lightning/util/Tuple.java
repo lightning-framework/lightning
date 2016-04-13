6640eda83763bbd4f1297d231704318cb0e3f422
mschurr <mschurr@rice.edu>
2016-04-12 20:06:14 -0500

    Update API.
    Signed-off-by: mschurr <mschurr@rice.edu>

--- src/main/java/lightning/util/Tuple.java
+++ src/main/java/lightning/util/Tuple.java
@@ -16,6 +16,37 @@ public class Tuple {
     private Tuple1D(T1 item) {
       _0 = item;
     }
+
+    @Override
+    public String toString() {
+      return "Tuple[" + _0 + "]";
+    }
+    
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((_0 == null) ? 0 : _0.hashCode());
+      return result;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      Tuple1D other = (Tuple1D) obj;
+      if (_0 == null) {
+        if (other._0 != null)
+          return false;
+      } else if (!_0.equals(other._0))
+        return false;
+      return true;
+    }
   }
   
   public static final class Tuple2D<T1, T2> {
@@ -25,6 +56,43 @@ public class Tuple {
     private Tuple2D(T1 item1, T2 item2) {
       _0 = item1;
       _1 = item2;
+    }
+    
+    @Override
+    public String toString() {
+      return "Tuple[" + _0 + ", " + _1 + "]";
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((_0 == null) ? 0 : _0.hashCode());
+      result = prime * result + ((_1 == null) ? 0 : _1.hashCode());
+      return result;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      Tuple2D other = (Tuple2D) obj;
+      if (_0 == null) {
+        if (other._0 != null)
+          return false;
+      } else if (!_0.equals(other._0))
+        return false;
+      if (_1 == null) {
+        if (other._1 != null)
+          return false;
+      } else if (!_1.equals(other._1))
+        return false;
+      return true;
     }
   }
   
@@ -37,6 +105,49 @@ public class Tuple {
       _0 = item1;
       _1 = item2;
       _2 = item3;
+    }
+    
+    @Override
+    public String toString() {
+      return "Tuple[" + _0 + ", " + _1 + ", " + _2 + "]";
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((_0 == null) ? 0 : _0.hashCode());
+      result = prime * result + ((_1 == null) ? 0 : _1.hashCode());
+      result = prime * result + ((_2 == null) ? 0 : _2.hashCode());
+      return result;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      Tuple3D other = (Tuple3D) obj;
+      if (_0 == null) {
+        if (other._0 != null)
+          return false;
+      } else if (!_0.equals(other._0))
+        return false;
+      if (_1 == null) {
+        if (other._1 != null)
+          return false;
+      } else if (!_1.equals(other._1))
+        return false;
+      if (_2 == null) {
+        if (other._2 != null)
+          return false;
+      } else if (!_2.equals(other._2))
+        return false;
+      return true;
     }
   }
   
@@ -51,6 +162,55 @@ public class Tuple {
       _1 = item2;
       _2 = item3;
       _3 = item4;
+    }
+    
+    @Override
+    public String toString() {
+      return "Tuple[" + _0 + ", " + _1 + ", " + _2 + ", " + _3 + "]";
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((_0 == null) ? 0 : _0.hashCode());
+      result = prime * result + ((_1 == null) ? 0 : _1.hashCode());
+      result = prime * result + ((_2 == null) ? 0 : _2.hashCode());
+      result = prime * result + ((_3 == null) ? 0 : _3.hashCode());
+      return result;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      Tuple4D other = (Tuple4D) obj;
+      if (_0 == null) {
+        if (other._0 != null)
+          return false;
+      } else if (!_0.equals(other._0))
+        return false;
+      if (_1 == null) {
+        if (other._1 != null)
+          return false;
+      } else if (!_1.equals(other._1))
+        return false;
+      if (_2 == null) {
+        if (other._2 != null)
+          return false;
+      } else if (!_2.equals(other._2))
+        return false;
+      if (_3 == null) {
+        if (other._3 != null)
+          return false;
+      } else if (!_3.equals(other._3))
+        return false;
+      return true;
     }
   }
   
